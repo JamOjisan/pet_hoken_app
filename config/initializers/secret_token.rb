b1b90e6d0a0e2fbb8321131dc3fd974e9830b342
S.I <urotansi2001@gmail.com>
2013-12-24 14:45:37 +0900

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PetHokenApp::Application.config.secret_key_base = 'b4d2806029a6ee7023f37504183fc141e4ebf49f3929ba7aaa39d311665a69ee54c1392a99befe84b7af962116dbc23ab5ee45248f4bf74b0189af7d739bd942'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PetHokenApp::Application.config.session_store :cookie_store, key: '_pet_hoken_app_session'
